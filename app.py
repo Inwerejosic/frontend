3034598d9d5ceebffaa99303895cafa6d927db23
Inweregbu, Joseph Chukwuka <Inwerejosic@users.noreply.github.com>
2025-02-07 22:41:29 +0000

    Now makinng fine

--- app.py
+++ app.py
@@ -83,4 +83,5 @@ def make_decision():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000) # host='0.0.0.0' is crucial for Azure VM
+     port = int(os.environ.get('PORT', 5000))  # Use PORT env or default to 5000
+    app.run(host='0.0.0.0', port=port)
